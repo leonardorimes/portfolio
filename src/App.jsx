af5d08fcdc3baac576b967ebc558112c2b18c6d2
Leonardo Rimes <rimesleo@gmail.com>
2024-01-15 23:43:14 -0300

    resolvendo caminho na imagem

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Portfolio from "./components/Portfolio";
 const projects = [
   {
     name: "React food",
-    url: "./src/assets/site_1.png",
+    url: "/src/assets/site_1.png",
     description:
       "Este projeto é uma landing page responsiva para um restaurante, desenvolvida utilizando React. A página é focada em oferecer uma experiência interativa e atraente para os visitantes",
     site_url: "https://react-food-landing-page.vercel.app",
@@ -22,14 +22,14 @@ const projects = [
   },
   {
     name: "Poke Search",
-    url: "./src/assets/site_3.png",
+    url: "/src/assets/site_3.png",
     description:
       "O Pokesearch é uma aplicação React que permite a busca e visualização de informações sobre Pokémons, utilizando a PokeAPI para obter dados completos sobre cada Pokémon, incluindo imagens, IDs, tipos, habilidades e estatísticas.",
     site_url: "https://pokesearch2023.netlify.app/",
   },
   {
     name: "FootSoccer Academy",
-    url: "./src/assets/site_4.png",
+    url: "/src/assets/site_4.png",
     description:
       "Esta é a landing page de uma escolinha de futebol, desenvolvida em Single-Page Application (SPA) utilizando JavaScript puro. A página conta com animações em CSS para criar uma experiência dinâmica e atraente.",
     site_url: "https://sage-sprite-595ef5.netlify.app/",
