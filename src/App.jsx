5ecc0546ccd210bad7e1c3959c0d06448f1939a1
Leonardo Rimes <rimesleo@gmail.com>
2024-01-15 23:56:20 -0300

    resolvendo caminho na imagem com url de um site

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import Portfolio from "./components/Portfolio";
 const projects = [
   {
     name: "React food",
-    url: "/src/assets/site_1.png",
+    url: "https://images2.imgbox.com/50/87/H67esZ8Z_o.png",
     description:
       "Este projeto é uma landing page responsiva para um restaurante, desenvolvida utilizando React. A página é focada em oferecer uma experiência interativa e atraente para os visitantes",
     site_url: "https://react-food-landing-page.vercel.app",
   },
   {
     name: "Rocket Notes",
-    url: "./src/assets/site_2.png",
+    url: "https://images2.imgbox.com/b9/1a/UxVBeZOk_o.png",
     description:
       "O Rocketnotes é um projeto fullstack concluído como parte do curso Explorer da Rocketseat. Durante os Stages 8 a 10, foram abordados diversos conceitos e práticas que me proporcionaram um aprendizado significativo.",
     site_url: "https://rocketnotes22.netlify.app/",
@@ -29,7 +29,7 @@ const projects = [
   },
   {
     name: "FootSoccer Academy",
-    url: "/src/assets/site_4.png",
+    url: "https://images2.imgbox.com/73/ff/zVUU7F0J_o.png",
     description:
       "Esta é a landing page de uma escolinha de futebol, desenvolvida em Single-Page Application (SPA) utilizando JavaScript puro. A página conta com animações em CSS para criar uma experiência dinâmica e atraente.",
     site_url: "https://sage-sprite-595ef5.netlify.app/",
