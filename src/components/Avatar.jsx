7ca39c17abd1b2428cb34fb4baaf06042b1dd8ca
Leonardo Rimes <rimesleo@gmail.com>
2025-07-05 11:38:11 -0300

    atualizando titulo

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -6,7 +6,7 @@ const Avatar = () => {
     <div className={styles.avatar}>
       <span>
         <h1>Leonardo Rimes</h1>
-        <p> Front - end Developer</p>
+        <p> Full Stack Developer</p>
       </span>
 
       <img src={Pic} className={styles.image} />
